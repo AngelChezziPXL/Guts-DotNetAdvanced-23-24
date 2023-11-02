ec7dc0ecbc3eb303b207379c3365eb7de5a5772e
arnobarzan <arno.barzan@pxl.be>
2023-11-02 10:27:53 +0100

    Update Question.cs
    Prevent SQLite bug in the tests.

--- 2_MVC-EF-NUnit/QuizApplication/QuizApplication.Domain/Question.cs
+++ 2_MVC-EF-NUnit/QuizApplication/QuizApplication.Domain/Question.cs
@@ -5,7 +5,12 @@ namespace QuizApplication.Domain
     public class Question
     {
         public int Id { get; set; }
+		
+		/* Please leave this MaxLength attribute on the QuestionString property, 
+         * the GUTS tests will fail if removed */
+        [MaxLength(100)]
         public string QuestionString;
+		
         public int CategoryId;
         public IList<Answer> Answers;
     }
